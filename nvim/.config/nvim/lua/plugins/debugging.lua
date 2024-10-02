2a2e3282c4bd33f11d9ea19ddaaa96e5066ee47d
CMDRBULB <fluorescentbulb2@gmail.com>
2024-10-02 13:20:00 -0500

    Arranged files for stow correctly this time

---
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -0,0 +1,10 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"leoluz/nvim-dap-go",
+		"rcarriga/nvim-dap-ui",
+    "tpope/vim-fugitive",
+    "folke/trouble.nvim",
+	},
+  config = function() end,
+}
